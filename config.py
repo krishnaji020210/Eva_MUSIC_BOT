6acba19dc402d36fc5b2f0cde2ada75d3e24e941
krishnaji020210 <krishnaji020210@gmail.com>
2026-03-20 07:03:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheAloneTeam/AloneMusic",
+    "https://github.com/krishnaji020210/Eva_MUSIC_BOT/blob/master/config.py#L47",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
